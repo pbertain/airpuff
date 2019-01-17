8b5f604b8e40a93353c448b12d9a351dda3c6e2d
Paul Bertain <paul@bertain.net>
2019-01-16 23:20:31 -0800

    adding in SQLite queries to get the phone numbers.

--- src/usr/local/bin/airpuff/airport-info-select-phones.py
+++ src/usr/local/bin/airpuff/airport-info-select-phones.py
@@ -7,7 +7,7 @@ icao               = sys.argv[1]
 conn               = sqlite3.connect(db_name)
 c                  = conn.cursor()
 c.execute("SELECT wx_phone FROM airports WHERE airport=?", (icao,))
-atis_phone         = c.fetchone()
+atis_phone         = "tel://+1-" + c.fetchone()
 print("%s" % atis_phone)
 
 conn.commit()
