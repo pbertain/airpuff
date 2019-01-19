fcf0595b226108b018caf33edbf39033d9c012d2
Paul Bertain <paul@bertain.net>
2019-01-19 00:55:56 -0800

    *actually* - using the telephone-wide image

--- src/usr/local/bin/airpuff/airpuff-tester.py
+++ src/usr/local/bin/airpuff/airpuff-tester.py
@@ -252,7 +252,7 @@ for count in range(0, met_json_results):
     else:
         print(textwrap.dedent("""\
         <tr class=\"td\">
-            <td><a href=\"%s\"><img width=20 height=20 src=\"/web/icons/telephone-wide-icon.png\"︎></a></td>
+            <td><a href=\"%s\"><img width=40 height=20 src=\"/web/icons/telephone-wide-icon.png\"︎></a></td>
             <td><img width=20 height=20 src=\"%s\"></td>
             <td><a class=\"%s\" href=\"/rrdweb/%s-rrd.html\">%-s</td>
             <td>%-s</td>
